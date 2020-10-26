ef3fb46e5879e88b2d2aff380286d1a42de73cde
Namik <ahmedov.namik@gmail.com>
2020-10-26 14:38:05 +0200

    added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
 import Footer from "./components/Footer/Footer"
 import "./components/Wrapper.css"
-function App() {
-  return (
-    <div id="wrapper">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div id="wrapper">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
 }
 export default App
